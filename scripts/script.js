ec8e96934b10e664291a90a977c3227be777fd86
Jordan Estes <jordanestes88@gmail.com>
2020-03-02 21:16:10 -0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+
+
+const myHeading2 = document.querySelector('h2');
+let myName = 'Jordan';
+myHeading2.textContent = 'My name is ' + myName;
